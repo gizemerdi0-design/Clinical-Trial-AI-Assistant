5b347387b47bf47631e52aaa1c0ba6ae7b33057c
gizemerdi0-design <gizemerdi0@gmail.com>
2026-04-21 07:17:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -576,15 +576,15 @@ if st.session_state.analysis_result:
                 if activities:
                     for act in activities:
                         st.markdown(f"""
-            <div style="
-                background-color:#eef2ff;
-                padding:8px 10px;
-                border-radius:8px;
-                margin-bottom:6px;
-            ">
-                • {act}
-            </div>
-             """, unsafe_allow_html=True)
+                        <div style='
+                            background-color:#eef2ff;
+                            padding:8px 10px;
+                            border-radius:8px;
+                            margin-bottom:6px;
+                        '>
+                        • {act}
+                        </div>
+                        """, unsafe_allow_html=True)
                  else:
                      st.markdown("• No activities extracted")
 
